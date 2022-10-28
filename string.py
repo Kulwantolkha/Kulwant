9c5b227955e1fc96eb56f8f363e95e2e5dba20ea
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2022-10-28 23:17:49 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,35 @@
+x="Python is a programming language"
+
+print(x[5])
+
+print(x[0:6])
+print(len(x))
+
+y= "Python"
+print(len(y))
+print(x[:4])
+
+print(x[2:])
+print(y[:])
+
+print(y[0:6:1])
+
+print(y[0:6:2])
+
+print(y[::3])
+
+print(y[-4:-2])
+print(y[::-1])
+print(y[-4:-1:-1])
+
+print(y[::-2])
+print(y[-4:-1])
+print(y[-4:-1:-1]) 
+print(y.isalnum())
+print(x.isalnum())
+print(x.endswith("language"))
+print(x.count("m")) 
+print(x.capitalize())
+print(x.find("is"))
+print(x.upper())
+print(x.replace("is","are"))
