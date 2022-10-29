725b14a7576f8a99accdbd57562a2e77fc8ebbd0
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2022-10-29 10:30:49 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,27 @@
+
+x='Hii'
+y= 7
+z= 32.3
+print(y+z)
+print(type(x))
+print(type(y))
+a= "34"
+c= "54"
+print(a+c)
+print(int(a)+int(c))
+print(10*"Hello World")
+print(10*"Hii\n")
+print(10*(int(a)+int(c)))
+print(10*int(a)+int(c))
+print(10*str(int(a)+int(c)))
+
+print("Enter your Number")
+x= int(input())
+print("You Entered", x+10)
+
+print("Enter first number")
+k=int(input())
+print("Enter second number")
+o=int(input())
+print("Here is your Result")
+print(k+o)
