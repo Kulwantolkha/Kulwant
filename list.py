18c56a574b2f2eb45352ab909aa2790b43e1b77e
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2022-10-30 00:58:40 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,30 @@
+grocery = ["Colgate", "Vim Bar", "Sabji", "Deodrant", 56]
+print(grocery)
+x=[11, 8, 12, 3, 20, 2]
+x.sort()
+print(x)
+x.reverse()
+print(x[0:5])
+print(x[::-1])
+x.append(16)
+print(x)
+x.insert(2,10)
+print(x)
+print(max(x))
+print(min(x))
+x.remove(20)
+print(x)
+x.pop()
+print(x)
+#x[11] - 19
+#print(x)
+# tp(10,19,16,25,1)
+# print(tp)
+a= 1
+b= 4
+temp = a
+a = b
+b = temp
+print(a,b)
+a,b=b,a
+print(a,b)
