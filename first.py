482477bd6f61f26bdae085e555b24f0c23057634
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2022-10-30 00:58:12 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,9 @@
+#This is a mutli Line Comment 
+"""this is 
+a milrti line comment"""
+print("Test File", end=".  ")  
+print("This is my first test file")
+print('C:\nulwant') 
+print("Kulwant is \n in \t library")
+
+ 
